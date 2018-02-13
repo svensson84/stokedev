b78b0026e9235ba205acbbcdcb8109be8493cdbc
svensson <sven.zaugg@stokedevelopment.ch>
2018-02-13 12:40:48 +0000

    initial project setup

---
+++ src/main/java/ch/stokedevelopment/stokedev/App.java
@@ -0,0 +1,13 @@
+package ch.stokedevelopment.stokedev;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
